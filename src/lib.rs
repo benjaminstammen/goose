1da165efb226136f3d937a82ca768d06b5343f36
Benjamin Stammen <x@benjaminstammen.com>
2022-04-18 22:19:42 +0000

    Implement B2 uploads via S3 client
    [Problem]
    Goslings are currently created, but are
    never uploaded.
    
    [Solution]
    Implement logic to create an S3 client, configure it
    for a given B2 bucket, and upload goslings to it.

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,12 @@
+use anyhow::Result;
+use aws_sdk_s3::types::ByteStream;
+use aws_sdk_s3::{Client, Config, Credentials, Endpoint, Region};
 use data_encoding::HEXLOWER;
 use ring::digest::{Context, Digest, SHA256};
-use std::fs;
 use std::fs::File;
-use std::io::{BufRead, BufReader, Read, Result};
+use std::io::{BufRead, BufReader, Read, Result as IOResult};
+use std::path::Path;
+use std::{env, fs};
 use uuid::Uuid;
 
 struct Gosling {
@@ -21,21 +25,28 @@ impl Gosling {
     }
 }
 
-pub fn file_gooser(file_path: &str, gosling_size: usize) {
-    let file = File::open(file_path).expect("Unable to open file!");
+pub async fn file_gooser(file_path: &str, gosling_size: usize) -> Result<()> {
+    let file = File::open(file_path)?;
 
     let mut goslings = Vec::new();
     let mut reader = BufReader::with_capacity(gosling_size, file);
+
+    let s3_client = build_client().await;
+
     loop {
         let length = {
             let buffer = reader.fill_buf().expect("Error reading file!");
-            goslings.push(spawn_gosling_for_buffer(buffer, file_path));
+            let gosling = spawn_gosling_for_buffer(buffer, file_path);
+            upload_file(&s3_client, &gosling).await?;
+            goslings.push(gosling);
             buffer.len()
         };
         if length == 0 {
             break;
         }
         reader.consume(length);
+        // TODO: Better progress indicator
+        print!(".");
     }
     println!("Finished spawning goslings.");
 
@@ -43,6 +54,7 @@ pub fn file_gooser(file_path: &str, gosling_size: usize) {
         "{}",
         sha256_digest_for_file(file_path).expect("Failed to compute hash")
     );
+    Ok(())
 }
 
 fn spawn_gosling_for_buffer(buffer: &[u8], write_path: &str) -> Gosling {
@@ -52,17 +64,54 @@ fn spawn_gosling_for_buffer(buffer: &[u8], write_path: &str) -> Gosling {
     Gosling::new(gosling_name, buffer.len(), gosling_path)
 }
 
-fn sha256_digest_for_file(file_path: &str) -> Result<String> {
+fn sha256_digest_for_file(file_path: &str) -> IOResult<String> {
     let input = File::open(file_path)?;
     let reader = BufReader::new(input);
     let digest = sha256_digest(reader)?;
     Ok(HEXLOWER.encode(digest.as_ref()))
 }
 
+async fn build_client() -> Client {
+    let access_key =
+        env::var("GOOSE_B2_ACCESS_KEY").expect("Need GOOSE_B2_ACCESS_KEY set for now.");
+    let secret_key =
+        env::var("GOOSE_B2_SECRET_KEY").expect("Need GOOSE_B2_SECRET_KEY set for now.");
+
+    // per https://nickb.dev/blog/access-public-and-private-b2-s3-buckets-in-rust, doesn't seem like provider matters.
+    let provider_name = "local-credentials";
+    let credentials = Credentials::new(&access_key, &secret_key, None, None, provider_name);
+
+    let b2_s3 = "https://s3.us-west-002.backblazeb2.com";
+    let b2_endpoint = Endpoint::immutable(b2_s3.parse().unwrap());
+
+    let config = Config::builder()
+        .region(Region::new("us-west-002"))
+        .endpoint_resolver(b2_endpoint)
+        .credentials_provider(credentials)
+        .build();
+
+    Client::from_conf(config)
+}
+
+async fn upload_file(client: &Client, gosling: &Gosling) -> Result<()> {
+    let bucket_name =
+        env::var("GOOSE_B2_UPLOAD_BUCKET").expect("Need GOOSE_B2_UPLOAD_BUCKET set for now.");
+    let body = ByteStream::from_path(Path::new(&gosling.file_path)).await;
+    client
+        .put_object()
+        .bucket(bucket_name)
+        .key(&gosling.id)
+        .body(body.unwrap())
+        .content_length(gosling.content_length as i64)
+        .send()
+        .await?;
+    Ok(())
+}
+
 // Lifted from the Rust Cookbook here: https://rust-lang-nursery.github.io/rust-cookbook/cryptography/hashing.html
 // TODO: find a way to compute the hash rapidly while creating goslings. Not doing so now because it's been
 //     prohibitively slow.
-fn sha256_digest<R: Read>(mut reader: R) -> Result<Digest> {
+fn sha256_digest<R: Read>(mut reader: R) -> IOResult<Digest> {
     let mut context = Context::new(&SHA256);
     let mut buffer = [0; 1024];
 
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 use goose::file_gooser;
 use std::env;
 
-fn main() {
+#[tokio::main]
+async fn main() {
     // 4MB seems like a decent choice for chunk size?
     const DEFAULT_GOSLING_SIZE: usize = 1024 * 1024 * 40;
 
     let args: Vec<String> = env::args().collect();
     let file_path = &args[1];
-    file_gooser(file_path, DEFAULT_GOSLING_SIZE);
+    file_gooser(file_path, DEFAULT_GOSLING_SIZE).await.expect("Failure.");
 }
